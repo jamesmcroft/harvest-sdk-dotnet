7093bb4041e9915c3a7dbe6f2f7be32ee2a0a4f5
James Croft <jamz_c@hotmail.co.uk>
2023-03-28 20:57:41 +0100

    Add capability to get a user details

--- src/Harvest/Users/UserEntryRequestBuilder.cs
+++ src/Harvest/Users/UserEntryRequestBuilder.cs
@@ -2,15 +2,17 @@ namespace Harvest.Users;
 
 using System;
 using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 using Common.Requests;
+using Models;
 
 /// <summary>
 /// Defines the builder for operations to manage a specific user.
 /// </summary>
 public class UserEntryRequestBuilder
 {
-    internal const string UserIdTemplateKey = "user%2Did";
-
     /// <summary>
     /// Initializes a new instance of the <see cref="UserEntryRequestBuilder"/> class with the specified path parameters and request adapter.
     /// </summary>
@@ -22,7 +24,7 @@ public class UserEntryRequestBuilder
         _ = pathParameters ?? throw new ArgumentNullException(nameof(pathParameters));
         _ = requestAdapter ?? throw new ArgumentNullException(nameof(requestAdapter));
 
-        this.UrlTemplate = $"{{+baseurl}}/users/{UserIdTemplateKey}";
+        this.UrlTemplate = "{+baseurl}/users/{+userid}";
         this.PathParameters = new Dictionary<string, object>(pathParameters);
         this.RequestAdapter = requestAdapter;
     }
@@ -41,4 +43,54 @@ public class UserEntryRequestBuilder
     /// Gets the URL template to use to build the request URL.
     /// </summary>
     private string UrlTemplate { get; }
+
+    /// <summary>
+    /// Retrieves a user.
+    /// </summary>
+    /// <remarks>
+    /// For more information: https://help.getharvest.com/api-v2/users-api/users/users/#retrieve-a-user
+    /// </remarks>
+    /// <param name="requestConfiguration">The configuration for the request such as headers.</param>
+    /// <param name="cancellationToken">The optional cancellation token.</param>
+    /// <returns>The user details.</returns>
+    /// <exception cref="HttpRequestException">Thrown when the request failed due to an underlying issue such as network connectivity, DNS failure, server certificate validation or timeout.</exception>
+    public async Task<User> GetAsync(Action<UserEntryRequestBuilderGetRequestConfiguration> requestConfiguration = default, CancellationToken cancellationToken = default)
+    {
+        RequestInformation requestInfo = this.ToGetRequestInformation(requestConfiguration);
+        return await this.RequestAdapter.SendAsync<User>(requestInfo, cancellationToken);
+    }
+
+    /// <summary>
+    /// Builds the request to retrieve a user.
+    /// </summary>
+    /// <param name="requestConfiguration">The configuration for the request such as headers.</param>
+    /// <returns>A request information object.</returns>
+    public RequestInformation ToGetRequestInformation(Action<UserEntryRequestBuilderGetRequestConfiguration> requestConfiguration)
+    {
+        var requestInfo = new RequestInformation
+        {
+            HttpMethod = Method.GET, UrlTemplate = this.UrlTemplate, PathParameters = this.PathParameters,
+        };
+
+        requestInfo.Headers.Add("User-Agent", "HarvestDotnetSdk");
+        requestInfo.Headers.Add("Accept", "application/json");
+
+        if (requestConfiguration == null)
+        {
+            return requestInfo;
+        }
+
+        var requestConfig = new UserEntryRequestBuilderGetRequestConfiguration();
+        requestConfiguration(requestConfig);
+        requestInfo.AddHeaders(requestConfig.Headers);
+        return requestInfo;
+    }
+
+    /// <summary>
+    /// Defines the configuration for the request to get a user.
+    /// </summary>
+    public class UserEntryRequestBuilderGetRequestConfiguration : BaseRequestConfiguration
+    {
+    }
 }
+
